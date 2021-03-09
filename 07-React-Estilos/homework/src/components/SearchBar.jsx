774ec65dcfc78d028ed775110aecf5a4f790a98f
Cristian Blandon <cristianblar12@icloud.com>
2021-03-09 17:21:17 -0500

    CP commiteado por cristianblar a las 1615328477020

--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React from "react";
+import busqueda from "./SearchBar.module.css";
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
-};
+  return (
+    <div>
+      <input
+        className={busqueda.barra}
+        type="text"
+        placeholder="Ciudad..."
+        id="buscar-ciudad"
+      />
+      <button
+        className={busqueda.boton}
+        onClick={() =>
+          props.onSearch(document.querySelector("#buscar-ciudad").value)
+        }
+      >
+        Agregar
+      </button>
+    </div>
+  );
+}
