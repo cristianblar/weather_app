7b2be5fc8c02f2937c6bf72bd8e08b8c165c9d1b
Cristian Blandon <cristianblar12@icloud.com>
2021-03-10 13:42:10 -0500

    Last commit with tailwind minified and working

--- 01-DOM/homework/postcss.config.js
+++ 01-DOM/homework/postcss.config.js
@@ -1,8 +1,31 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
+const purgeHtml = require("purgecss-from-html");
+const purgeJs = require("purgecss-from-js");
 
 module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
+    purgecss({
+      content: ["./*.html", "./*.js"],
+      options: {
+        extractors: [
+          {
+            extractor: purgeJs,
+            extensions: ["js"],
+          },
+          {
+            extractor: purgeHtml,
+            extensions: ["html"],
+          },
+        ],
+      },
+      safelist: {
+        standard: [/^[wh]-\d+\/\d+$/],
+      },
+    }),
+    require("cssnano")({
+      preset: "default",
+    }),
   ],
 };
