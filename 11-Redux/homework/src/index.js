df750b862e9ec1da862b831569cdaaad6b618a01
Cristian Blandon <cristianblar12@icloud.com>
2021-03-15 19:29:34 -0500

    CP commiteado por cristianblar a las 1615854574348

--- 11-Redux/homework/src/index.js
+++ 11-Redux/homework/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import Counter from './components/Counter';
 import counter from './reducers';
 
@@ -10,15 +11,19 @@ import counter from './reducers';
 // que es responsable de la actualización del store , junto
 //con cualquier estado inicial con el que queramos que
 //empiece el store (que en este caso es ninguno).
-const store = createStore(counter);
+const initialState = {
+	count: 0,
+};
+
+const store = createStore(counter, initialState, devToolsEnhancer());
 
 // Aquí, envolvemos nuestro componente principal React dentro de las etiquetas del Provider,
 // que vienen del paquete react-redux.
-// Esto es necesario porque el store necesita saber hacia dónde está pasando su estado. 
+// Esto es necesario porque el store necesita saber hacia dónde está pasando su estado.
 // El componente Provider es donde "vive" el store.
 ReactDOM.render(
-  <Provider store={store}>
-    <Counter />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<Counter />
+	</Provider>,
+	document.getElementById('root')
 );
